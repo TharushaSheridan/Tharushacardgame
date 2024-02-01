4c538408ee937ad5485d3952a100fff4ef7300bf
TharushaSheridan <157389423+TharushaSheridan@users.noreply.github.com>
2024-01-31 22:22:27 -0500

    Update new CardTrick.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -12,7 +12,7 @@ package card;
  * @modifier Tharusha Liyanage
  */
 import java.util.Scanner;
-//comment
+//comment ppp
 public class CardTrick {
     
     public static void main(String[] args) {
